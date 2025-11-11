7fc2b964aac1322bb4a5c83ee1d3e1abec2eaef4
AltoulV <artvincent38@gmail.com>
2025-11-11 19:46:04 +0100

    update 11/11/2025

--- pages/index.js
+++ pages/index.js
@@ -45,9 +45,8 @@ export default function Accueil() {
           title="La pédagogie"
           subtitle=""
           highlights={[
-            {illustration:'/images/VivreE.png', title:'Le vivre ensemble', text:'Mise en place de créneaux qui se chevauchent pour qu\' il y ait plus d\’interactions pendant les cours, organisation d\'événements internes pendant l\'année: rencontre parents/enfants, compétition par équipe anciens/nouveaux...'},
+            {illustration:'/images/VivreE.png', title:'Le vivre ensemble', text:'organisation d\'événements internes pendant l\'année: rencontre parents/enfants, compétition par équipe anciens/nouveaux...'},
             {illustration:'/images/RoleMini.png', title:'Les différents rôles', text:'Le maître d\'armes donnera des progressions écritent dans les différents rôles de l\'escrimeur: Tireur, Mini-maître, Arbitre, et Bénévole. Les enfants auront une référence pour leurs progressions.'},
-            {illustration:'/images/feedback.png', title:'Un retour des séances sur le Blog', text:'Le maître d\'armes écrira régulièrement sur le blog un retour sur les avancements des élèves, les parents pourront donc suivre à distance le déroulement des cours et l\'avancement des enfants !'},
           ]}
         />
 
@@ -58,7 +57,7 @@ export default function Accueil() {
           features={[
             {illustration:'/images/MarcFranco.png', title:'Marc FRANCO', text:'Président'},
             {illustration:'/images/OdileChemineau.png', title:'Odile CHEMINEAU', text:'Trésorière'},
-            {illustration:'/images/FabienHallez.png', title:'Fabien HALLEZ', text:'Secrétaire'},
+            {illustration:'/images/BrunoRoustant.png', title:'Bruno ROUSTANT', text:'Secrétaire'},
             {illustration:'/images/ArthurV.png', title:'Arthur VINCENT', text:'Maître d\'armes'},
           ]}
         />
